9e31f726dd20f44b819a8922ffa7ec9476b922ca
Santeri Paajanen <paajanensanteri@gmail.com>
2023-03-17 10:21:38 +0200

    Updated run scripts

--- run/run_training.sh
+++ run/run_training.sh
@@ -3,7 +3,7 @@
 set -e
 
 
-time python ../src/train_model.py -e 50 -n 1 --pretrained
+time python ../src/train_model.py -e 100 -n 1 -wd 0 --pretrained
 
 exit
 
--- run/sbatch_launch.sh
+++ run/sbatch_launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH -p gpu
-#SBATCH -t 15:00:00
-#SBATCH --mem=5000
+#SBATCH -t 17:00:00
+#SBATCH --mem=6000
 #SBATCH --gres=gpu:v100:1
 #SBATCH -J IDL_fp
 #SBATCH -o output.txt
@@ -14,4 +14,4 @@ module purge
 module load pytorch/1.13
 
 #python ../src/train_model.py  --maxepoch 200 -n  ${SLURM_ARRAY_TASK_ID} --slurm-mode
-python ../src/train_model.py -e 30 -n 2 --pretrained --train-all -lr 0.00005 -wd 0 --slurm-mode
+python ../src/train_model.py -e 100 -n 3 --pretrained --train-all -lr 0.0001 -wd 0 --slurm-mode
