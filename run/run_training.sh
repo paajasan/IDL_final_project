05e2866153146a7dfc31ffce39a7bfb643a4830f
Santeri Paajanen <paajanensanteri@gmail.com>
2023-03-31 11:15:04 +0300

    Final training

--- run/run_training.sh
+++ run/run_training.sh
@@ -3,10 +3,10 @@
 set -e
 
 
-time python ../src/train_model.py -e 100 -n 1 -wd 0 --pretrained
+time python ../src/train_model.py -e 100 -n 6 -wd 0 -lr 0.0001 --pretrained
 
 exit
 
-for ((i=1; i<12; i++)); do
-    python ../src/train_model.py -s 30 -e 30 -n  $i
+for ((i=1; i<7; i++)); do
+    python ../src/train_model.py -s 40 -e 50 -wd 0.0005 -n  $i
 done
